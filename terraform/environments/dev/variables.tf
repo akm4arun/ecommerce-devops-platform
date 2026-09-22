fd401033d1c9f59c6288204aeba64e8539d5746c
Arun Mishra <arunkumarmishra058@gmail.com>
2026-09-22 23:19:57 +0530

    feat: provision development vpc networking

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -9,3 +9,21 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the development VPC"
+  type        = string
+  default     = "10.20.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+  default     = "10.20.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability Zone for the public subnet"
+  type        = string
+  default     = "ap-south-2a"
+}
